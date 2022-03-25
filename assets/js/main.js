79663f4783ec45c929b15e3b036f14335297c55b
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-25 15:11:14 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
 document.querySelectorAll('.rate').forEach(rate => {
     rate.addEventListener('click', () => {
+        document.querySelectorAll('.rate').forEach(rateElem => {
+            rateElem.classList.remove('selected')
+        })
         rate.classList.toggle('selected')
     })
 })
@@ -8,9 +11,9 @@ const sectionThank = document.querySelector('#main-section-thank')
 sectionThank.style.display = 'none'
 
 document.querySelector('#submit').addEventListener('click', () => {
-    const elements = document.querySelectorAll('.rate.selected')
+    const elements = document.querySelector('.rate.selected')
     sectionThank.style.display = 'block'
     document.querySelector('#main-section').style.display = 'none'
     const feedback = document.querySelector('#feedback')
-    feedback.textContent = `You selected ${elements.length} out of 5`
+    feedback.textContent = `You selected ${elements.textContent} out of 5`
 })
